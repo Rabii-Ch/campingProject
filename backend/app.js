56d831cdde6fbb8763c7ba0f3fe961ca2b30dfb1
slim bellalah <slimbellalah@yahoo.com>
2020-12-10 19:25:13 +0100

    app

--- backend/app.js
+++ backend/app.js
@@ -8,3 +8,5 @@ app.use(bodyParser.urlencoded({ extended: true }));
 const mongoose = require('mongoose');
 
 const path = require('path');
+
+mongoose.connect('mongodb://localhost:27017/campingDB', { useNewUrlParser: true, useUnifiedTopology: true });
