0859ed9cac1925701cb6ffe65f63e74ece6d90cb
slim bellalah <slimbellalah@yahoo.com>
2020-12-10 18:49:30 +0100

    ajout backend

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './component/home/home.component';
+import { LoginComponent } from './component/login/login.component';
 import { NotFoundComponent } from './component/not-found/not-found.component';
+import { SignUpComponent } from './component/sign-up/sign-up.component';
 
 const routes: Routes = [
   {path:'', component: HomeComponent},
+  {path:'sign-up', component: SignUpComponent},
+  {path:'login', component: LoginComponent},
   {path:'**', component: NotFoundComponent}
 ];
 
