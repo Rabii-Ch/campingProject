1aee2fdfaa1ec552fb9f1df66f447622ce08d852
kinggara <rabii_chandra@hotmail.fr>
2020-12-12 14:20:35 +0100

    guide

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -22,6 +22,7 @@ import { EditCircuitComponent } from './component/edit-circuit/edit-circuit.comp
 import { DisplayCircuitComponent } from './component/display-circuit/display-circuit.component';
 import { PannierComponent } from './component/pannier/pannier.component';
 import { MaterielComponent } from './component/materiel/materiel.component';
+import { GuidesComponent } from './component/guides/guides.component';
 
 
 @NgModule({
@@ -43,7 +44,8 @@ import { MaterielComponent } from './component/materiel/materiel.component';
     EditCircuitComponent,
     DisplayCircuitComponent,
     PannierComponent,
-    MaterielComponent
+    MaterielComponent,
+    GuidesComponent
   ],
   imports: [
     BrowserModule,
